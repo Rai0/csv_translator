90970d93fb813fa96d3af6773ee9ef024551c383
Rai0 <43114095+Rai0@users.noreply.github.com>
2022-07-30 15:30:30 +0300

    hotfix

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import time
 import gc
 
 class connectCSV:
-    
+
     @classmethod
-    def read_file(cls) -> list[list]:
-        endpint: str = "positions_and_skills.csv"
-        table_from_endpint_csv_file = []
-        with codecs.open(endpint, "r", "utf_8_sig") as csv_file:
+    def read_file(cls, endpoint: str) -> list[list]:
+        if endpoint.find(".scv"): endpoint += ".csv"
+        table_from_endpoint_csv_file = []
+        with codecs.open(endpoint, "r", "utf_8_sig") as csv_file:
             read_csv_file = csv.reader(csv_file)
             for row in read_csv_file:
-                table_from_endpint_csv_file.append(row)
-        return table_from_endpint_csv_file
+                table_from_endpoint_csv_file.append(row)
+        return table_from_endpoint_csv_file
 
     @classmethod
     def writer_file(cls, list_for_writer: list, new_file_name: str = "eng_positions_and_skills") -> None:
         new_file_name += ".csv"
         try:
-            with open(new_file_name, "a") as csv_file:
-                write_scv_file = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            with open(new_file_name, "a", newline='') as csv_file:
+                write_scv_file = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
                 write_scv_file.writerow(list_for_writer)
         except Exception as _ex:
             print("[ERROR] some one do wrong:\n", _ex)
@@ -43,12 +43,12 @@ class translator:
                 time_list = []
                 for item_id in range(0, len(data_list[row_id])):
                     item = data_list[row_id][item_id]
-                    if len(optimiztion_dict) > 1000:
-                        optimiztion_dict.clear()
                     if item_id < self.columns:
-                        if optimiztion_dict.get (item) == None:
+                        if not optimiztion_dict.get (item):
                             optimiztion_dict.update ({item : self.example_of_translator.translate(item, dest=self.to_lang).text})
                         time_list.append (optimiztion_dict.get (item))
+                        if len(optimiztion_dict) > 1000:
+                            optimiztion_dict.clear()
                     else: 
                         time_list.append(item)
                 connectCSV.writer_file(time_list)
@@ -58,4 +58,4 @@ class translator:
         return "[INFO] congratulations, congratulations, congratulations"
 
 if __name__ == '__main__':
-    translator().translate(connectCSV.read_file())
+    translator().translate(connectCSV.read_file("test"))
